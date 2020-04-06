6250b6a7aa5e3b84d3b89bd203f4f9045f1574d6
Patrick Pin <patrickpin840@hotmail.com>
2020-04-06 15:01:44 -0400

    [R2] Implement Photon PUN

---
+++ Assets/Scripts/Photon/PhotonLobby.cs
@@ -0,0 +1,28 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonLobby : MonoBehaviourPunCallbacks
+{
+    public static PhotonLobby lobby;
+
+    private void Awake()
+    {
+        lobby = this;
+    }
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsVisible = false;
+        roomOptions.MaxPlayers = 2;
+        PhotonNetwork.JoinOrCreateRoom("Room", roomOptions, TypedLobby.Default);
+    }
+}
