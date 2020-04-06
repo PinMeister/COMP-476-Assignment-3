6d262a5bf7fb4d8bca5020de66c7715df875f6c3
Patrick Pin <patrickpin840@hotmail.com>
2020-04-06 18:05:37 -0400

    [R1] Converted level to fit A3 requirements

---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 30;
+    [SerializeField] float rotateSpeed = 100;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("Vertical") < 0)
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        if (Input.GetAxisRaw("Vertical") > 0)
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        if (Input.GetAxisRaw("Horizontal") < 0)
+            transform.Rotate(-Vector3.up * rotateSpeed * Time.deltaTime);
+        if (Input.GetAxisRaw("Horizontal") > 0)
+            transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+    }
+}
